6eb09b8bb652471b460df9aa6c3883c45ebadc1c
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-08-07 14:38:16 +0100

    updated

--- Auth-backend/passport.js
+++ Auth-backend/passport.js
@@ -6,7 +6,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "http://localhost:3000/auth/google/callback",
+			callbackURL: "/auth/google/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
