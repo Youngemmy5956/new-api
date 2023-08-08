08e3b154e66cb23b9d7c531721ef4167e48087d0
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-08-08 16:32:59 +0100

    added

--- booking-backend/index.js
+++ booking-backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import connectDB from "./config/mongo.js";
 import cors from "cors";
+import userRouter from "./routes/user-routes";
 
 
 
@@ -15,6 +16,14 @@ app.use(
     origin: "http://localhost:3001",
   })
 );
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
 
 async function connect() {
   try {
