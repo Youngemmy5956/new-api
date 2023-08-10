0c76362a6668a728d22247e454ec7221de59396b
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-08-10 10:28:36 +0100

    added

--- booking-backend/controllers/movie-controller.js
+++ booking-backend/controllers/movie-controller.js
@@ -8,7 +8,7 @@ export const addMovie = async (req, res, next) => {
     return res.status(404).json({ message: "Token Not Found" });
   }
 
-  let adminId;
+  // let adminId;
 
   // verify token
   jwt.verify(extractedToken, process.env.SECRET_KEY, (err, decrypted) => {
