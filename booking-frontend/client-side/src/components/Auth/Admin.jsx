c114e081ada670aa65bb6aa6804038e1a7e9885d
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-08-15 08:52:13 +0100

    updated

--- booking-frontend/client-side/src/components/Auth/Admin.jsx
+++ booking-frontend/client-side/src/components/Auth/Admin.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { sendAdminAuthRequest } from "../../api-helpers/api-helpers";
+import { sendAdminAuthRequest } from "../Auth/"
 import { adminActions } from "../../store";
 import AuthForm from "./AuthForm";
 
