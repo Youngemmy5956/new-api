c2c66543f96db8e95ff13cce9b393136b8d6fa5b
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-09-01 01:19:39 +0100

    added

--- booking-backend/controllers/message-controller.js
+++ booking-backend/controllers/message-controller.js
@@ -1,19 +1,26 @@
-try {
-    const { name, email, subject, message } = req.body; // Added 'message' here
-    if (!email || email.trim() === "") {
-      return res.status(400).json({ error: "Email address is required." });
-    }
+import Message from "../models/Message.js";
+import nodemailer from "nodemailer";
 
-    // Save the email to MongoDB
-    const emailRecord = new Email({ name, email, subject, message }); // Added 'message' here
-    await emailRecord.save();
+export const createMessage = async (req, res) => {
+  const { name, email, subject } = req.body;
 
-    // Send the email
+  // Check if email is not defined or empty
+  if (!email || email.trim() === "") {
+    return res.status(400).json({ error: "Email address is required." });
+  }
+  try {
+    await Message.create({
+      name,
+      email,
+      subject,
+    });
+    // Create a Gmail transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         type: "OAuth2",
         user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         refreshToken: process.env.REFRESH_TOKEN,
@@ -21,17 +28,25 @@ try {
     });
 
     const mailOptions = {
-      from: process.env.MAIL_USERNAME, // Use your Gmail email address
       to: "recipient@example.com", // Your email where you want to receive the message
       subject: subject,
-      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`, // Added 'message' here
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${subject}`,
     };
 
     await transporter.sendMail(mailOptions);
 
     res.status(200).json({ message: "Message sent successfully!" });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    if (error.code === "EAUTH") {
+      // Authentication error (invalid email/password)
+      res
+        .status(401)
+        .json({
+          error: "Authentication failed. Check your email and password.",
+        });
+    } else {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 };
