84dd0ac504cc15ed6e7a592f6da60bedabe25d36
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-09-01 01:38:50 +0100

    added

--- booking-backend/controllers/message-controller.js
+++ booking-backend/controllers/message-controller.js
@@ -1,8 +1,21 @@
 import Message from "../models/Message.js";
 import nodemailer from "nodemailer";
 
+// Create a Gmail transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
 export const createMessage = async (req, res) => {
-  const { name, email, subject } = req.body;
+  const { name, email, subject, message } = req.body; // Include 'message' here
 
   // Check if email is not defined or empty
   if (!email || email.trim() === "") {
@@ -13,25 +26,14 @@ export const createMessage = async (req, res) => {
       name,
       email,
       subject,
-    });
-    // Create a Gmail transporter
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-      },
+      message, // Include 'message' here
     });
 
     const mailOptions = {
       from: "Mech_Api_Form",
       to: "emmanuelgodwin558@gmail.com", // Your email where you want to receive the message
-      subject: " Contact Form Submission",
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${subject}`,
+      subject: "Contact Form Submission",
+      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`, // Include 'message' here
     };
 
     await transporter.sendMail(mailOptions);
